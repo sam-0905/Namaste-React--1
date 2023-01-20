4d922579a2335824209f1e1711b5d8b8458709f5
AllanSam <allansam0905@gmail.com>
2023-01-20 21:53:47 +0530

    #

--- session-3/coding/index.js
+++ session-3/coding/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "Hello World !"
-);
-
 // Functional Component
-const headerComponent = () => {
-  return <h1> Namamste react Functional Component </h1>;
+const HeaderComponent = function () { // Normal function
+  return (
+    <div>
+      <h1> Namamste react Functional Component </h1>
+      <h2></h2>
+    </div>
+  );
 };
 
+const HeaderComponent1 = () => { // Arrow function
+  return (
+    <div>
+      <h1> Namamste react Functional Component </h1>
+      <h2></h2>
+    </div>
+  );
+};
+// these both are one and the same. this is a arrow function thing.In advanced
+const HeaderComponent2 = () => (
+  <div>
+    <h1> Namamste react Functional Component </h1>
+    <h2></h2>
+  </div>
+);
+
+//React.createElement => Object =>Html(DOM)
 //const heading = React.createElement("h1", {
 //        id: "title",
 //        key: "h1",
@@ -22,9 +35,13 @@ const headerComponent = () => {
 //    "Hello World !"
 //)
 
-// JSX?
+// JSX? React.createElement => Object =>Html(DOM) //** react element */
 const head5 = (
   <h1 id="title" key="h1">
     Namamste React{" "}
   </h1>
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(head5);
