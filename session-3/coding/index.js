2562d73b2de9d012a07e7d721a4a7fd1dd9fed43
AllanSam <allansam0905@gmail.com>
2023-01-20 18:44:53 +0530

    #

--- session-3/coding/index.js
+++ session-3/coding/index.js
@@ -8,12 +8,24 @@ const heading = React.createElement("h1", {
     "Hello World !"
 )
 
+// Functional Component
+const headerComponent = () => {
+    return <h1 > Namamste react Functional Component < /h1>
+}
+
+//const heading = React.createElement("h1", {
+//        id: "title",
+//        key: "h1",
+//    },
+//    "Hello World !"
+//)
 
 // JSX?
 //const head5 = (
 //<h1 id = "title" key="h1">
 // Namamste React 
 // </h1>) 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
