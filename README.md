1c482180ebad94869af8a93c420a3e4ea06f4102
AllanSam <allansam0905@gmail.com>
2023-01-21 09:03:06 +0530

    updated

--- README.md
+++
@@ -1,4 +0,0 @@
-# Namaste-React -1
- 
- This repo contains all the classes done as part of Namaste React course by Akshay Saini
-
--- session-3/coding/index.js
+++ session-3/coding/index.js
@@ -1,40 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Functional Component
-const HeaderComponent = function () { // Normal function
-  return (
-    <div>
-      <h1> Namamste react Functional Component </h1>
-      <h2></h2>
-    </div>
-  );
-};
-
-const HeaderComponent1 = () => { // Arrow function
-  return (
-    <div>
-      <h1> Namamste react Functional Component </h1>
-      <h2></h2>
-    </div>
-  );
-};
-// these both are one and the same. this is a arrow function thing.In advanced
-const HeaderComponent2 = () => (
-  <div>
-    <h1> Namamste react Functional Component </h1>
-    <h2></h2>
-  </div>
-);
-
-//React.createElement => Object =>Html(DOM)
-//const heading = React.createElement("h1", {
-//        id: "title",
-//        key: "h1",
-//    },
-//    "Hello World !"
-//)
-
 // JSX? React.createElement => Object =>Html(DOM) //** react element */
 const head5 = (
   <h1 id="title" key="h1">
@@ -42,6 +8,49 @@ const head5 = (
   </h1>
 );
 
+const HeaderComponent1 = () => {
+  // Arrow function
+  return (
+    <div>
+      {head5}
+      <h2> Namamste react Functional Component </h2>
+      <h3>This is a h2 tag</h3>
+    </div>
+  );
+};
+
+// Functional Component
+//const HeaderComponent = function () {   // Normal function
+//  return (
+//    <div>
+//      <h1> Namamste react Functional Component </h1>
+//      <h2>This is a h2 tag</h2>
+//    </div>
+//  );
+//};
+
+// these both are one and the same. This is a arrow function thing.In advanced
+//const HeaderComponent2 = () => (
+//  <div>
+//    <h1> Namamste react Functional Component </h1>
+//    <h2>This is a h2 tag</h2>
+//  </div>
+//);
+
+//React.createElement => Object =>Html(DOM)
+
+//const heading = React.createElement("h1", {
+//        id: "title",
+//        key: "h1",
+//    },
+//    "Hello World !"
+//)
+
+// passing a ReactElement inside root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// async defer
 root.render(head5);
+
+// passing  a Functional component inside root
+root.render(<HeaderComponent1 />);
