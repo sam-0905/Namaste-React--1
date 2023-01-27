4e0e9d23bebaf48e92966bd7b475c2cf3a9477aa
AllanSam <allansam0905@gmail.com>
2023-01-27 11:48:42 +0530

    updated logo & css part
    Signed-off-by: AllanSam <allansam0905@gmail.com>

---
+++ session-4/coding-4/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Logo from "./icons/logo-chef-restaurant.svg";
+
+// JSX? React.createElement => Object =>Html(DOM) //** react element */
+
+const Title = () => (
+  <div id="title2" key="h1" className="brand">
+    <img className="logo" src={Logo} alt="logo" />
+    <h3 className="brandName"> The Chef in the Hat</h3>
+  </div>
+);
+
+// Functional Component
+
+const HeaderComponent = function () {
+  // Normal function
+  return (
+    <div className="header">
+      <Title />
+
+      <div className="Nav-container">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// Component Composition
+
+//const Title7 = () => (
+//  <h1 id="tile7" key="h1">
+//    Namamste react
+//  </h1>
+//);
+
+//const HeaderComponent = function () { // Normal function
+//  return (
+//    <div>
+//      <Title7 />
+//      <h1> Namamste react Functional Component </h1>
+//      <h2>This is a h2 tag</h2>
+//    </div>
+//  );
+//};
+
+// passing a ReactElement inside root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing  a Functional component inside root
+//root.render(<PrintElements />);
+root.render(<HeaderComponent />);
