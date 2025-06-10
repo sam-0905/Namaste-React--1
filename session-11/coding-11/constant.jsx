3889a1ed4924f41e9254dbcac15e223907c968e9
Sam <118877482+sam-0905@users.noreply.github.com>
2025-06-10 18:36:42 +0530

    updated main api url to constant

--- session-11/coding-11/constant.jsx
+++ session-11/coding-11/constant.jsx
@@ -5,6 +5,8 @@
 export const IMG_CDN_URl =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
+export const MAIN_API_URL ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
 export const FETCH_MENU_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.931948400167405&lng=80.1345556229353&restaurantId=";
 
