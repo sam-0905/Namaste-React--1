1edc028875d244be36c3861c386df8b7dfcf69fa
Sam <118877482+sam-0905@users.noreply.github.com>
2025-06-30 21:49:46 +0530

    feat - updated media url

--- session-11/coding-11/constant.jsx
+++ session-11/coding-11/constant.jsx
@@ -1,9 +1,10 @@
 // These are Hardcoded data.So once we eport it import form anywhere.
 
 // Named import and import {} from"";
+export const REACT_MEDIA_URL = "https://media-assets.swiggy.com/swiggy/"
 
 export const IMG_CDN_URl =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const MAIN_API_URL ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
