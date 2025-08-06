35cad351a46c3d7df6d90b6a07751b602aae2bea
Sam <118877482+sam-0905@users.noreply.github.com>
2025-08-06 17:54:08 +0530

    updated

--- session-11/src/pages/Contact.jsx
+++ session-11/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Contact.css";
+import "./contact.css";
 
 const Contact = () => {
     const contact = new URL("../assets/contact.png", import.meta.url).href;
