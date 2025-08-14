c4126e9e7b889661b4d7554578bf6c7cc0ea378b
Sam <118877482+sam-0905@users.noreply.github.com>
2025-08-14 22:56:34 +0530

    Added logic and css for userOffline component

--- session-11/src/components/userOffline.jsx
+++ session-11/src/components/userOffline.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import OfflineImg from "../assets/offlinePic.png" ; // update the path if needed
+import "./userOffline.css";
 
 function UserOffline() {
+  const OfflineImg = new URL("../assets/offlinePic.png", import.meta.url).href;
+
   return (
-    <div className="offline-container">
-      <h1>🥵 Oops! Looks like you are offline</h1>
-      <img className="offline-image" src={OfflineImg} alt="offline" />
-      <p className="offline-message">
-        Check your internet connection and try again. Your cravings are waiting!
-      </p>
-      <button
+      <>
+       <div className="offline-container">
+        <h1>🥵 Oops! Looks like you are offline</h1>
+        <img className="offline-image" src={OfflineImg} alt="offline" />
+        <p className="offline-message">
+          Check your internet connection and try again. Your cravings are waiting!
+        </p>
+       <button
         className="offline-retry-btn"
         onClick={() => window.location.reload()}
-      >
+         >
         Retry
-      </button>
+       </button>
     </div>
+      </>
   );
 }
 
