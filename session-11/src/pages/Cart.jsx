fd2f33bbcdb66a1ef3ee59de58dad647f7dc92bf
Sam <118877482+sam-0905@users.noreply.github.com>
2025-11-22 22:15:48 +0530

    feat - created cart page and updated with some css

--- session-11/src/pages/Cart.jsx
+++ session-11/src/pages/Cart.jsx
@@ -1,11 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
+import {removeItem ,clearCart} from "../utils/cartSlice";
+import "./cart.css";
+import { REACT_MEDIA_URL } from "../constant";
+import { IMG_MENU_URL } from "../constant";
 
-const cart = () => {
+const Cart = () => {
+  
+  const cartItems = useSelector(store => store.cart.items);
+  console.log(cartItems)
+  const dispatch = useDispatch();
+  const CDN_URL = IMG_MENU_URL;
+  
 
-    return <>
-        <div>
-            <h1>This is a cart page</h1>
+  return (
+    <>
+      <div className="cart-container">
+  <header className="cart-header">
+    <h1>This is a cart page</h1>
+    <div className="cart-count">Cart Items - <span>{cartItems.length}</span></div>
+  </header>
+
+  <div className="cart-list">
+    {cartItems.map(item => (
+        
+      <div className="cart-item" key={item.id}>
+        <div className="item-image"> {/* optional image placeholder */}
+           <img src={REACT_MEDIA_URL + CDN_URL + item.imageId}  alt={item.name} />            
         </div>
-        </>
-}
 
-export default cart;
+        <div className="item-info">
+          <h2 className="item-name">{item.name}</h2>
+          <p className="item-desc">{item.description}</p>
+          <div className="item-meta">
+            <div className="item-price">₹ {(item.price||0)/100}</div>
+            <div className="item-rating">⭐ {item.ratings?.aggregatedRating?.rating || "–"} <span>({item.ratings?.aggregatedRating?.ratingCountV2})</span></div>
+          </div>
+        </div>
+
+        <div className="item-actions">
+          <button className="qty-btn" onClick={() => dispatch(removeItem(item.id))}>-</button>
+          <button className="clear-btn" onClick={() => dispatch(clearCart())}>Clear</button>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
+
+    </>
+  );
+};
+
+export default Cart;
