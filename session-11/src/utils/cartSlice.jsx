6fa4b9a59a0f3a2e301993d103283fb3dd7b067f
Sam <118877482+sam-0905@users.noreply.github.com>
2025-12-15 18:04:20 +0530

    updated cartSlice reducer with more logic for addItem and removeItem

--- session-11/src/utils/cartSlice.jsx
+++ session-11/src/utils/cartSlice.jsx
@@ -5,17 +5,48 @@ const cartSlice = createSlice({
     initialState:{
         items:[],
     },
-    reducers:{
-        addItem:(state, action) => {
-            state.items.push(action.payload)
-        },
-        removeItem:(state,action) => {
-            state.items.pop(action.payload)
-        },  
-        clearCart:(state) =>{
-            state.items = [];
-        }
+ 
+    reducers: {
+  addItem: (state, action) => {
+    const item = action.payload;
+
+    const existingItem = state.items.find(
+      (i) => i.id === item.id
+    );
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      state.items.push({
+        ...item,
+        quantity: 1,
+      });
     }
+  },
+
+  removeItem: (state, action) => {
+    const id = action.payload;
+
+    const existingItem = state.items.find(
+      (i) => i.id === id
+    );
+
+    if (existingItem) {
+      if (existingItem.quantity > 1) {
+        existingItem.quantity -= 1;
+      } else {
+        state.items = state.items.filter(
+          (item) => item.id !== id
+        );
+      }
+    }
+  },
+
+  clearCart: (state) => {
+    state.items = [];
+  },
+}
+
 })
 
 export const{addItem,removeItem,clearCart} = cartSlice.actions
